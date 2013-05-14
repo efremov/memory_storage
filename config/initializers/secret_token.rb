c5acdb9aa99cf93d6cfe5f57bd59d716b427ee33
efremov <eco933@rambler.ru>
2013-05-14 13:53:47 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemoryStorage::Application.config.secret_token = '64bfa660f7025a094d5560c4722d8dce7912c80c204df16a2323ccf8ff222f7a9e5cd793f730cbfc00c0b531b68ee74c1bfa7f915077fc5f920f6745253495bf'
